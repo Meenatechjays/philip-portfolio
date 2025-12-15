ffe85a5f7d2b3f5b8ca456fd938835a605a0f4dc
Meenatechjays <meena.sivakumar@techjays.com>
2025-12-15 16:16:31 +0530

    Basic Setup Completed

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enable the React Compiler for better performance and DX
+  reactCompiler: true,
+};
+
+export default nextConfig;
+
+
